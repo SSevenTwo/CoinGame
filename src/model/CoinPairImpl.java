7fabba22d30bf1b678864c3956ec0eff498b91c4
Ian Nguyen <iannguyen172@gmail.com>
2019-08-24 00:29:07 +1000

    Revert "Implemented exception handling"
    This reverts commit 60cc0e80fa34f2de9401602177882f8b8078bf03.

--- src/model/CoinPairImpl.java
+++ src/model/CoinPairImpl.java
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 package model;
 
 import java.util.Objects;
@@ -64,3 +65,71 @@ public class CoinPairImpl implements CoinPair {
 	
 
 }
+=======
+package model;
+
+import java.util.Objects;
+
+import model.interfaces.Coin;
+import model.interfaces.CoinPair;
+
+public class CoinPairImpl implements CoinPair {
+	
+	private CoinImpl coin1;
+	private CoinImpl coin2;
+	
+	//Constructor for CoinPair
+	public CoinPairImpl() {
+		this.coin1 = new CoinImpl(1);
+		this.coin2 = new CoinImpl(2);
+	}
+
+	@Override
+	//Returns coin 1
+	public Coin getCoin1() {
+		return this.coin1;
+	}
+
+	@Override
+	//Returns coin 2
+	public Coin getCoin2() {
+		return this.coin2;
+	}
+
+	@Override
+	// Compares the two faces of the coins to check if they are equal
+	public boolean equals(CoinPair coinPair) {
+		if(this.coin1.equals(coinPair.getCoin1()) && this.coin2.equals(coinPair.getCoin2())){
+			return true;
+		}
+		return false;
+	}
+	
+	@Override
+	// Ensures that the object is actually a coinpair first
+	public boolean equals(Object obj) {
+		if(obj instanceof CoinPair) {
+			return this.equals(obj);
+		}
+		else return false;
+	}
+	
+	@Override
+	// Return details of coinpair as string
+	public String toString() {
+		String coin = String.format("%s, %s", this.coin1,this.coin2); 
+		return coin;
+	}
+
+	@Override
+	// Creates hashcode based on the faces of the two coins
+	public int hashCode() {
+		return Objects.hash(coin1.getFace(),coin2.getFace());
+	}
+
+
+	
+	
+
+}
+>>>>>>> 2dfa711cf3d1d10e2791007630dbc0af3987f8fe
